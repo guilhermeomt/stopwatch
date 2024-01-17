215498e74fd169561e714fe488a14a9c4de59444
Guilherme Tavares <contato@guilhermeomt.dev>
2024-01-17 09:40:10 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const StopwatchApp());
+}
+
+class StopwatchApp extends StatelessWidget {
+  const StopwatchApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
